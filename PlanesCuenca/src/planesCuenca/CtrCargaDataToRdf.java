f0fb43ad705bd91aff262a26ee0a12537952ffd8
Fabian <falpema@gmail.com>
2019-03-05 21:08:48 -0500

    Se procede a agregar la configuracion de primefaces

--- PlanesCuenca/src/planesCuenca/CtrCargaDataToRdf.java
+++ PlanesCuenca/src/planesCuenca/CtrCargaDataToRdf.java
@@ -1,5 +1,7 @@
 package planesCuenca;
+import javax.faces.application.FacesMessage;
 import javax.faces.bean.*;
+import javax.faces.context.FacesContext;
 
 
 
@@ -19,6 +21,10 @@ public class CtrCargaDataToRdf {
 
 	public void setTitulo(String titulo) {
 		this.titulo = titulo;
+	}
+	
+	public void CargarRdfRestaurantes(){
+		 FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_INFO, "Info", "Cargar de Datos correcta."));
 	}
 
 }
