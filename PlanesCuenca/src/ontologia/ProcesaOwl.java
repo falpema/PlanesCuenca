79d8cf41059f968c84a73d522f071d64ce9685c0
tatymunoz <tatymunoz@localhost>
2019-03-13 00:01:53 -0500

    Cambio para manejo de rdf

--- PlanesCuenca/src/ontologia/ProcesaOwl.java
+++ PlanesCuenca/src/ontologia/ProcesaOwl.java
@@ -1,7 +1,11 @@
 package ontologia;
+import java.io.File;
 import java.io.FileWriter;
 import java.io.InputStream;
+import java.io.PrintWriter;
 
+import org.apache.jena.ontology.DatatypeProperty;
+import org.apache.jena.ontology.Individual;
 import org.apache.jena.ontology.OntClass;
 import org.apache.jena.ontology.OntModel;
 import org.apache.jena.ontology.OntModelSpec;
@@ -13,6 +17,11 @@ import org.apache.jena.query.ResultSet;
 import org.apache.jena.query.ResultSetFormatter;
 import org.apache.jena.rdf.model.Model;
 import org.apache.jena.rdf.model.ModelFactory;
+import org.apache.jena.rdf.model.Property;
+import org.apache.jena.rdf.model.RDFNode;
+import org.apache.jena.rdf.model.Resource;
+import org.apache.jena.rdf.model.Statement;
+import org.apache.jena.rdf.model.StmtIterator;
 import org.apache.jena.util.iterator.ExtendedIterator;
 
 import cargaCSV.cargaCSVtoRDF;
@@ -20,39 +29,175 @@ import cargaCSV.cargaCSVtoRDF;
 import org.apache.jena.util.FileManager;
 
 public class ProcesaOwl {
+	
+	static String NS = "http://www.semanticweb.org/usuario/ontologies/2019/2/ruta#";
+	
 	public static void carga() {
 		
 		String userdir= System.getProperty("user.dir");
 		
 		OntModel model = null;	 
+		OntClass bares = null;
 		model = ModelFactory.createOntologyModel(OntModelSpec.OWL_MEM);	 
-		model.read(userdir+ "/src/recursos/ontologia_ruta_final.owl","RDF/XML"); 
+		model.read(userdir+ "/src/recursos/ontologia_general.owl","RDF/XML"); 
 		 
 		System.out.println( "Clases/Instancias");
 		System.out.println( "=================");
 		ExtendedIterator iteratorClasses = model.listClasses(); 
 		while ( iteratorClasses.hasNext() ){
 		    OntClass ontClass = (OntClass) iteratorClasses.next();
+		    
+		    if(ontClass.getLocalName().equals("Discoteca"))
+		    {
+		    	 bares = ontClass;	
+		    }
+		    
 		    System.out.println( ontClass );		 
 		    ExtendedIterator iteratorInstances = ontClass.listInstances();
 		    while ( iteratorInstances.hasNext() ){
-		   System.out.println( "\t"+iteratorInstances.next() );
+		       System.out.println( "\t"+iteratorInstances.next() );
 		    }
-		    System.out.println("");
+		    
 		}
+		    System.out.println("");
+		    
+		    DatatypeProperty nombre = model.getDatatypeProperty("http://www.semanticweb.org/usuario/ontologies/2019/2/ruta#nombre");
+		   
+		    DatatypeProperty precio = model.getDatatypeProperty("http://www.semanticweb.org/usuario/ontologies/2019/2/ruta#precio");
+		    
+		    DatatypeProperty latitud = model.getDatatypeProperty("http://www.semanticweb.org/usuario/ontologies/2019/2/ruta#latitud");
+		    
+		    DatatypeProperty longitud = model.getDatatypeProperty("ttp://www.semanticweb.org/usuario/ontologies/2019/2/ruta#longitud");
+		    
+		    
+		    
+		    Model modelrdf = FileManager.get().loadModel("/home/tatymunoz/Documentos/resultado.rdf", null, "RDF/XML");
+		 	
+		    StmtIterator iter = modelrdf.listStatements();
+		 	
+		    Integer contador=0;
+		    String valor="";
+		    String valorlatitud="";
+		    String valorlongitud="";
+		    String valorprecio="";
+		    String valornombre="";
+		    String nombrerecurso="Bar";
+		    
+		    Individual instancia = null;
+		    while (iter.hasNext()) {
+		 	    
+		    	contador++;
+		    	Statement stmt      = iter.nextStatement();  // get next statement
+		 	    Resource  subject   = stmt.getSubject();     // get the subject
+		 	    Property  predicate = stmt.getPredicate();   // get the predicate
+		 	    RDFNode   object    = stmt.getObject();      // get the object
+
+		 	   
+		 	    System.out.println(predicate.getLocalName());
+		 	    System.out.print(subject.toString());
+		 	    System.out.print("predicado" + predicate.toString() + " ");
+		 	    System.out.print("uri" + predicate.getURI() + " ");
+		 	    
+		 	   	    
+		 	    
+		 	    if (object instanceof Resource) {
+		 	    	valor=object.toString();
+		 	       System.out.print("recurso "+object.toString());
+		 	    } else {
+		 	        // object is a literal
+		 	    	valor="\"" + object.toString() + "\"";
+		 	        System.out.print("\"" + object.toString() + "\"");
+		 	    }
+		 	   
+
+		 	   switch (predicate.getLocalName())
+		 	   {
+		 	   
+		 	    case "Nombre" :
+		 		 nombrerecurso=object.toString().replace(" ", "").toUpperCase();
+		 		 valornombre=valor.toString();
+		 		 break;
+		 	    case "Latitud":
+		 	     valorlatitud=valor;
+		 	     break;
+		 	    case "Longitud":
+		 	     valorlongitud=valor;
+			 	 break;
+		 	    case "Precio":
+			 	 valorprecio=valor;
+				 break;  
+		 	   }
+		 	  
+		 	  int resto = contador%14;
+		 	  
+		 	  if(resto==0)
+		 	  {
+		 		instancia = model.createIndividual(NS+nombrerecurso,bares);
+		 		instancia.setPropertyValue(nombre, model.createTypedLiteral(valornombre));
+		 		instancia.setPropertyValue(latitud, model.createTypedLiteral(valorlatitud));
+		 		instancia.setPropertyValue(precio, model.createTypedLiteral(valorprecio));
+		 		//instancia.setPropertyValue(longitud, model.createTypedLiteral(valorlongitud));
+		 	  }
+		 	  
+		 	  
+		 	} 
+		    
+		    		    
+		     
+		     try
+		     {
+		     
+		   //Almacenamos la ontología en un fichero OWL (Opcional)
+		     File file = new File("/home/tatymunoz/Documentos/ONTOLOGIA/Animales3.owl");
+		     //Hay que capturar las Excepciones
+		     if (!file.exists()){
+		          file.createNewFile();
+		     }
+		     model.write(new PrintWriter(file));
+		     
+		     }
+		     catch(Exception e)
+		     {
+		    	 
+		     }
+		
+		
+		
+		
 		Consulta(model);
 	}
 	public static void Consulta(OntModel model) {
-		//RESULTADOS CON SPARQL							 			
-		String queryString = 
-		   "PREFIX po: <http://www.semanticweb.org/usuario/ontologies/2019/2/ruta#> "+
-		   "SELECT ?nf "+
-		   "WHERE  "+
-		   "{  "+
-		   "   po:r1 po:tieneFloreria ?nombre. "+
-		   "   ?nombre po:nombre ?nf "+
-		   "}";		 		 
-		Query query = QueryFactory.create(queryString);		 
+//		//RESULTADOS CON SPARQL							 			
+//		String queryString = 
+//		   "PREFIX po: <http://www.semanticweb.org/usuario/ontologies/2019/2/ruta#> "+
+//		   "SELECT ?nf "+
+//		   "WHERE  "+
+//		   "{  "+
+//		   "   po:r1 po:tieneFloreria ?nombre. "+
+//		   "   ?nombre po:nombre ?nf "+
+//		   "}";	
+		
+		
+		String SparQlIndividual=
+	    	     "prefix ns:<http://www.semanticweb.org/usuario/ontologies/2019/2/ruta#>"+
+	             "prefix rdfs:<http://www.w3.org/2000/01/rdf-schema#>"+
+	             "prefix rdf:<http://www.w3.org/1999/02/22-rdf-syntax-ns#>"+
+	             "SELECT * { { " +
+	             "select ?precio "+
+	             "where { "
+	             + "?dataP rdf:type ns:Discoteca . "
+	             + "?dataP ns:nombre ?precio . }  } " 
+				 + " UNION { "
+				 + "SELECT ?precio  "
+				 + "WHERE  "
+				 + "{  "
+		         + "   ns:r1 ns:tieneFloreria ?nombre. "
+		         + "   ?nombre ns:nombre ?precio "
+		         + "} } }";	
+	        
+		
+		
+		Query query = QueryFactory.create(SparQlIndividual);		 
 		// Ejecutar la consulta y obtener los resultados
 		QueryExecution qe = QueryExecutionFactory.create(query, model);		 
 		try {
